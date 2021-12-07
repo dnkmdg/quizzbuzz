2c538c45fbbb45bb2f983a72440a5426f70204d0
Daniel Källstrand Modig <daniel@ikoncept.se>
2021-12-07 11:47:26 +0100

    Enving firther

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const http = require('https')
 const express = require('express')
 const cors = require('cors')
 const socketio = require('socket.io')
+const api = require('./src/api')
 const app = express()
 
 const pkey = process.env.PKEY ? fs.readFileSync(process.env.PKEY) : null
@@ -34,70 +35,6 @@ const io = socketio(server, {
     }
 })
 
-const data = {
-    host: null,
-    users: new Set(),
-    buzzes: new Set()
-}
-
-const getData = () => ({
-    users: [...data.users],
-    buzzes: [...data.buzzes].map(b => {
-        const [name, team] = b.split('-')
-        return { name, team }
-    })
-})
-
-const isExistingUser = (user) => {
-    const users = [...data.users].map(u => u.id)
-    return users.includes(user.id)
-}
-
-app.get('/api/host/:hostId', (req, res) => {
-    if (data.host && data.host !== req.params.hostId) {
-        res.sendStatus(401)
-        return
-    }
-
-    data.host = req.params.hostId
-    res.json(getData())
-})
-
-io.on('connection', (socket) => {
-    socket.on('join', (user) => {
-        if (!isExistingUser(user)) {
-            data.users.add(user)
-            console.log(`${user.name} joined!`)
-        } else {
-            console.log(`${user.name} rejoined!`)
-        }
-
-        io.emit('active', [...data.users])
-    })
-
-    socket.on('buzz', (user) => {
-        data.buzzes.add(`${user.name}`)
-        io.emit('buzzes', [...data.buzzes])
-        console.log(`${user.name} buzzed in!`)
-    })
-
-    socket.on('clear', () => {
-        data.buzzes = new Set()
-        io.emit('buzzes', [...data.buzzes])
-        console.log('Clear buzzes')
-    })
-
-    socket.on('quit', () => {
-        data.buzzes = new Set()
-        data.users = new Set()
-        data.host = null
-
-        io.emit('buzzes', [...data.buzzes])
-        io.emit('active', [...data.users])
-        io.emit('quit')
-
-        console.log('Quit game')
-    })
-})
+api.init(app, io)
 
 server.listen(8090, () => console.log('Listening on 8090'))
